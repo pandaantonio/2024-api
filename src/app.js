461a25398fbc5f174e08dc2f6663ef560c5d2bcb
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-10-05 02:32:11 -0300

    ok

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const boardRoutes = require('./board');
+const express = require("express");
+const boardRoutes = require("./board");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/api/board', boardRoutes);
+app.user("/", (req, res) => {
+  res.send(200);
+});
+app.use("/api/board", boardRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
