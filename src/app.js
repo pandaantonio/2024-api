de59fa6e4d64def37b1b2f19b6c196c8fb87b3d9
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-10-05 02:39:48 -0300

    errei denovo

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ const boardRoutes = require("./board");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use("/", (req, res) => {
-  res.send(200);
-});
 app.use("/api/board", boardRoutes);
 
 app.listen(PORT, () => {
